0891c63db8a9867e00531f0acae695cb96b5d5ed
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-05-18 20:24:13 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ParentComponent from './ParentComponent';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ParentComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
