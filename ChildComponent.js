5491f15aa31f25adc27b39c38dc84173998fac67
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-05-18 19:55:36 +0100

    Create ChildComponent.js

---
+++ ChildComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChildComponent = () => {
+  return (
+    <div>
+      <h3>Child Component</h3>
+      <p>This is a child component</p>
+    </div>
+  );
+};
+
+export default ChildComponent;
